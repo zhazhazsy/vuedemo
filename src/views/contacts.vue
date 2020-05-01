68753a420bece3f6b2e64b07d1b008e8f04ed99c
zhazhazsy <1006942332@qq.com>
2020-05-01 13:31:25 +0800

    5-1 BUG修复

--- src/views/contacts.vue
+++ src/views/contacts.vue
@@ -45,7 +45,7 @@
 			  this.userid = sessionStorage.getItem("user");
 			  const ths = this;
 			  //请求后端数据
-			  this.axios.get('http://127.0.0.1:8081/user/nome?user='+this.userid).then(function (res){
+			  this.axios.get('http://47.113.112.177:8081/user/nome?user='+this.userid).then(function (res){
 			  	console.log(res)
 			  	ths.contacts = res.data
 			  })
